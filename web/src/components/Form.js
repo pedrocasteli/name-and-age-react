4ea4b67a0ad32ac4da54fd29f182de8dc94308d0
pedroCasteli <pedrocasteli68@gmail.com>
2022-08-29 14:36:01 -0300

    Laying the foundation for a modal.

--- web/src/components/Form.js
+++ web/src/components/Form.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useReducer } from "react";
+import Modal from "./Modal.js";
 
 import checkIcon from "../assets/check.svg";
 import crossIcon from "../assets/cross.svg";
@@ -14,6 +15,7 @@ const formReducer = (state, action) => {
             };
         case "API_RETURN_USERNAME":
             return {
+                ...state,
                 usernameValue: action.payload.match
                     ? action.payload.username
                     : "",
@@ -28,6 +30,7 @@ const formReducer = (state, action) => {
                 emailValue: action.payload.value,
                 emailIsValid: action.payload.isValid,
                 formIsValid: action.payload.isValid && state.usernameIsValid,
+                apiReturned: false,
             };
         default:
             return {
@@ -43,6 +46,7 @@ const formReducer = (state, action) => {
 
 const Form = () => {
     const [isLoading, setIsLoading] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
 
     const [formState, dispatchForm] = useReducer(formReducer, {
         usernameValue: "",
@@ -69,6 +73,7 @@ const Form = () => {
                             type: "API_RETURN_USERNAME",
                             payload: data,
                         });
+                        console.log(data);
                     });
             }, 500);
 
@@ -90,74 +95,82 @@ const Form = () => {
         });
     };
 
+    const submitFormHandler = (e) => {
+        e.preventDefault();
+    };
+
     return (
-        <form className="form">
-            <section className="form__section">
-                <label htmlFor="username">Usuário</label>
-                <div className="form__search">
-                    <input
-                        id="username"
-                        name="username"
-                        type="text"
-                        defaultValue={formState.usernameValue}
-                        onChange={changeUsernameHandler}
-                        className="form__input"
-                        required
-                    ></input>
-                    {isLoading && (
-                        <img
-                            src={spinnerIcon}
-                            alt={"Spinner icon"}
-                            height={32}
-                            width={32}
-                            className="form__image form__image--spinner"
-                        />
-                    )}
-                    {!isLoading && formState.usernameIsValid && (
-                        <img
-                            src={checkIcon}
-                            alt={"Check icon"}
-                            height={20}
-                            width={20}
-                            className="form__image"
-                        />
-                    )}
-                    {!isLoading &&
-                        formState.apiReturned &&
-                        !formState.usernameIsValid && (
+        <React.Fragment>
+            <Modal open={isOpen} onClose={() => setIsOpen(false)} />
+            <form className="form" onSubmit={submitFormHandler}>
+                <section className="form__section">
+                    <label htmlFor="username">Usuário</label>
+                    <div className="form__search">
+                        <input
+                            id="username"
+                            name="username"
+                            type="text"
+                            defaultValue={formState.usernameValue}
+                            onChange={changeUsernameHandler}
+                            className="form__input"
+                            required
+                        ></input>
+                        {isLoading && (
                             <img
-                                src={crossIcon}
-                                alt={"Cross icon"}
+                                src={spinnerIcon}
+                                alt={"Spinner icon"}
+                                height={32}
+                                width={32}
+                                className="form__image form__image--spinner"
+                            />
+                        )}
+                        {!isLoading && formState.usernameIsValid && (
+                            <img
+                                src={checkIcon}
+                                alt={"Check icon"}
                                 height={20}
                                 width={20}
                                 className="form__image"
                             />
                         )}
-                </div>
-            </section>
-            <section className="form__section">
-                <label htmlFor="age">E-mail</label>
-                <div className="form__search">
-                    <input
-                        id="email"
-                        name="email"
-                        type="email"
-                        defaultValue={formState.emailValue}
-                        onChange={changeEmailHandler}
-                        className="form__input"
-                        disabled={!formState.usernameIsValid}
-                        required
-                    ></input>
-                </div>
-            </section>
-            <button
-                type="submit"
-                className="form__button"
-                disabled={!formState.formIsValid}
-            >
-                Enviar
-            </button>
-        </form>
+                        {!isLoading &&
+                            formState.apiReturned &&
+                            !formState.usernameIsValid && (
+                                <img
+                                    src={crossIcon}
+                                    alt={"Cross icon"}
+                                    height={20}
+                                    width={20}
+                                    className="form__image"
+                                />
+                            )}
+                    </div>
+                </section>
+                <section className="form__section">
+                    <label htmlFor="age">E-mail</label>
+                    <div className="form__search">
+                        <input
+                            id="email"
+                            name="email"
+                            type="email"
+                            defaultValue={formState.emailValue}
+                            onChange={changeEmailHandler}
+                            className="form__input"
+                            disabled={!formState.usernameIsValid}
+                            required
+                        ></input>
+                    </div>
+                </section>
+                <button
+                    type="submit"
+                    className="form__button"
+                    disabled={!formState.formIsValid}
+                    onClick={() => setIsOpen(!isOpen)}
+                >
+                    Enviar
+                </button>
+            </form>
+        </React.Fragment>
     );
 };
 
---
+++ web/src/components/Modal.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import okGIF from "../assets/ok.gif";
+import styles from "./Modal.module.scss";
+
+const Modal = (props) => {
+    return props.open
+        ? ReactDOM.createPortal(
+              <div className={styles.backdrop} onClick={props.onClose}>
+                  <div className={styles.modal}>
+                      <img
+                          src={okGIF}
+                          alt="GIF of a man giving the camera 'thumbs up'."
+                          width={282}
+                          height={286}
+                      ></img>
+                      <button onClick={props.onClose}>Voltar</button>
+                  </div>
+              </div>,
+              document.getElementById("portal")
+          )
+        : null;
+};
+
+export default Modal;
